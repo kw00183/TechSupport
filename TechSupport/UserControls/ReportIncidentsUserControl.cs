099567987aaf9f963f7ae0844027540ec4fbc556
Kim Weible <kw00183@my.westga.edu>
2022-03-21 01:24:32 -0500

    add in XML comments to ReportIncidents user control

--- TechSupport/UserControls/ReportIncidentsUserControl.cs
+++ TechSupport/UserControls/ReportIncidentsUserControl.cs
@@ -3,12 +3,26 @@ using System.Windows.Forms;
 
 namespace TechSupport.UserControls
 {
+    /// <summary>
+    /// user control class that encapsulates the report open incidents assigned controls
+    /// Author: Kim Weible
+    /// Version: Spring 2022
+    /// </summary>
     public partial class ReportIncidentsUserControl : UserControl
     {
+        #region Constructors
+
+        /// <summary>
+        /// constructor used to create the report incident controls
+        /// </summary>
         public ReportIncidentsUserControl()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Data Methods
 
         private void AssignedOpenIncidentsReportViewer_VisibleChanged(object sender, EventArgs e)
         {
@@ -17,5 +31,7 @@ namespace TechSupport.UserControls
 
             this.assignedOpenIncidentsReportViewer.RefreshReport();
         }
+
+        #endregion
     }
 }
